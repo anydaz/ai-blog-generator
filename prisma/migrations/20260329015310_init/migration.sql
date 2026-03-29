04c8f5bbf3419c4a4af54f8221e9e6c0a509975e
andy darmawan <andy.daz.95@gmail.com>
2026-03-29 09:53:34 +0800

    add migration

---
+++ prisma/migrations/20260329015310_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "summary" TEXT NOT NULL,
+    "topic" TEXT NOT NULL,
+    "research" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'draft',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "publishedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Post_slug_key" ON "Post"("slug");
